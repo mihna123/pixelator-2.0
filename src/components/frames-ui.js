e59d5aee63c9cb1754df5e637b3a150bf35b2a27
mihna123 <mihailonvojinovic@gmail.com>
2025-03-06 17:52:17 +0100

    Add up and down frame moves

--- src/components/frames-ui.js
+++ src/components/frames-ui.js
@@ -1,7 +1,9 @@
 import { getFramesState, getState } from "../core/state/shared-state.js";
 import { createNewLayer } from "../core/utils/helpers.js";
 import copyIcon from "../icons/copy-icon.js";
+import downArrowIcon from "../icons/down-arrow-icon.js";
 import thrashIcon from "../icons/thrash-icon.js";
+import upArrowIcon from "../icons/up-arrow-icon.js";
 
 /** @type {HTMLDivElement}*/
 const framesContainer = document.getElementById("frames-container");
@@ -43,6 +45,11 @@ function copyFrame(e) {
 /** @param {MouseEvent} e */
 function removeFrame(e) {
 	const state = getState();
+	if (state.frames.length === 1) {
+		alert("At least one frame must exist!");
+		return;
+	}
+
 	const framesState = getFramesState();
 
 	/** @type {HTMLButtonElement} */
@@ -69,6 +76,99 @@ function removeFrame(e) {
 	}
 }
 
+/** @param {MouseEvent} e */
+function moveFrameUp(e) {
+	e.stopPropagation();
+	const state = getState();
+	if (state.frames.length === 1) return;
+
+	const framesState = getFramesState();
+
+	/** @type {HTMLButtonElement} */
+	const btn = e.currentTarget;
+
+	/** @type {HTMLDivElement} */
+	const divContainer = btn.parentElement;
+
+	// Skip the button as it is the last element
+	for (let i = 0; i < framesContainer.children.length - 1; ++i) {
+		if (!framesContainer.children[i].isSameNode(divContainer)) continue;
+		if (i <= 0) return;
+		//const leftPart = state.frames.slice(0, i);
+		const leftPart = state.frames.slice(0, i - 1);
+		//	const rightPart = state.frames.slice(i + 2, undefined);
+		const rightPart = state.frames.slice(i + 1, undefined);
+		const leftFrameCopy = JSON.parse(JSON.stringify(state.frames[i - 1]));
+		const rightFrameCopy = JSON.parse(JSON.stringify(state.frames[i]));
+		leftPart.push(rightFrameCopy, leftFrameCopy, ...rightPart);
+
+		state.frames = leftPart;
+		state.shouldClear = true;
+		state.shouldDraw = true;
+		state.frameIndex = i - 1;
+
+		const firstDiv = framesContainer.children[i - 1];
+		const secondDiv = framesContainer.children[i];
+
+		framesState.canvas = secondDiv.firstChild;
+
+		framesContainer.insertBefore(secondDiv, firstDiv);
+
+		for (let c of framesContainer.children) {
+			c.children[0].classList.remove("border-amber-300");
+		}
+		secondDiv.firstElementChild.classList.add("border-amber-300");
+
+		break;
+	}
+}
+/** @param {MouseEvent} e */
+function moveFrameDown(e) {
+	e.stopPropagation();
+	const state = getState();
+	if (state.frames.length === 1) return;
+
+	const framesState = getFramesState();
+
+	/** @type {HTMLButtonElement} */
+	const btn = e.currentTarget;
+
+	/** @type {HTMLDivElement} */
+	const divContainer = btn.parentElement;
+
+	// Skip the button as it is the last element
+	for (let i = 0; i < framesContainer.children.length - 1; ++i) {
+		if (!framesContainer.children[i].isSameNode(divContainer)) continue;
+
+		if (i >= state.frames.length - 1) return;
+
+		const leftPart = state.frames.slice(0, i);
+		const rightPart = state.frames.slice(i + 2, undefined);
+		const leftFrameCopy = JSON.parse(JSON.stringify(state.frames[i]));
+		const rightFrameCopy = JSON.parse(JSON.stringify(state.frames[i + 1]));
+		leftPart.push(rightFrameCopy, leftFrameCopy, ...rightPart);
+
+		state.frames = leftPart;
+		state.shouldClear = true;
+		state.shouldDraw = true;
+		state.frameIndex = i + 1;
+
+		const firstDiv = framesContainer.children[i];
+		const secondDiv = framesContainer.children[i + 1];
+
+		framesState.canvas = firstDiv.firstChild;
+
+		framesContainer.insertBefore(secondDiv, firstDiv);
+
+		for (let c of framesContainer.children) {
+			c.children[0].classList.remove("border-amber-300");
+		}
+		firstDiv.firstElementChild.classList.add("border-amber-300");
+
+		break;
+	}
+}
+
 /**
  * Adds a new frame canvas to the canvas container
  * @param {Object} options
@@ -86,22 +186,36 @@ function addNewFrameCanvas({ nodeToInsertBeforeOf }) {
 	const canvas = document.createElement("canvas");
 	const copyFrameBtn = document.createElement("button");
 	const removeFrameBtn = document.createElement("button");
+	const frameUpBtn = document.createElement("button");
+	const frameDownBtn = document.createElement("button");
 
-	canvasDiv.className = "w-full max-h-20";
+	canvasDiv.className = "w-full max-h-20 overflow-y-clip";
 	canvasDiv.appendChild(canvas);
 	canvasDiv.appendChild(copyFrameBtn);
 	canvasDiv.appendChild(removeFrameBtn);
+	canvasDiv.appendChild(frameUpBtn);
+	canvasDiv.appendChild(frameDownBtn);
 
 	copyFrameBtn.innerHTML = copyIcon;
 	copyFrameBtn.className =
-		"relative left-[48px] -top-[70px] flex justify-center items-center bg-gray-900/65 text-white/65 hover:bg-gray-800 hover:text-white p-1";
+		"relative left-[48px] -top-[70px] flex justify-center items-center bg-gray-900/50 text-white/65 hover:bg-gray-800 hover:text-white p-1";
 
 	removeFrameBtn.innerHTML = thrashIcon;
 	removeFrameBtn.className =
 		"relative left-[48px] -top-[52px] flex justify-center items-center bg-gray-900/50 text-white/65 hover:bg-gray-800 hover:text-white p-1";
 
+	frameUpBtn.innerHTML = upArrowIcon;
+	frameUpBtn.className =
+		"relative left-[2px] -top-[118px] flex justify-center items-center bg-gray-900/50 text-white/65 hover:bg-gray-800 hover:text-white p-1";
+
+	frameDownBtn.innerHTML = downArrowIcon;
+	frameDownBtn.className =
+		"relative left-[2px] -top-[100px] flex justify-center items-center bg-gray-900/50 text-white/65 hover:bg-gray-800 hover:text-white p-1";
+
 	copyFrameBtn.onclick = copyFrame;
 	removeFrameBtn.onclick = removeFrame;
+	frameDownBtn.onclick = moveFrameDown;
+	frameUpBtn.onclick = moveFrameUp;
 
 	canvas.classList.add("border-2", "border-amber-300");
 	canvas.width = 70;
---
+++ src/icons/up-arrow-icon.js
@@ -0,0 +1,5 @@
+const upArrowIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512">
+    <path fill="currentColor" d="M247.5 0L34.2 213.3v128l170.6-170.6V512h85.4V170.7l170.6 170.6v-128z"/>
+    </svg>`;
+
+export default upArrowIcon;
